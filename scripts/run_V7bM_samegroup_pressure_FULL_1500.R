e3025c4b0d167c9a72d8d27c5f69d1b02221221d
Dr Dave Hudson <64405890+davehudson67@users.noreply.github.com>
2026-09-10 16:51:19 +0100

    Use numeric-dummy fix for full same-group pressure run

--- scripts/run_V7bM_samegroup_pressure_FULL_1500.R
+++ scripts/run_V7bM_samegroup_pressure_FULL_1500.R
@@ -3,6 +3,6 @@ Sys.setenv(
   N_KEEP="100",
   MIN_GROUP_N="1",
   SEED="7092042",
-  RESULT_TAG="FULL_1500_V2"
+  RESULT_TAG="FULL_1500_V3"
 )
-source("scripts/Woodchester_V7bM_samegroup_pressure_models_V2.R")
+source("scripts/Woodchester_V7bM_samegroup_pressure_models_NUMERIC_DUMMIES.R")
