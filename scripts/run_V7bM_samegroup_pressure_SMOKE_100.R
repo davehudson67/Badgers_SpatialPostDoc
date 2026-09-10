4431e8cd212eb57d13da03bcfba86b0c0ff8fff1
Dr Dave Hudson <64405890+davehudson67@users.noreply.github.com>
2026-09-10 16:51:10 +0100

    Use numeric-dummy fix for same-group pressure smoke run

--- scripts/run_V7bM_samegroup_pressure_SMOKE_100.R
+++ scripts/run_V7bM_samegroup_pressure_SMOKE_100.R
@@ -3,6 +3,6 @@ Sys.setenv(
   N_KEEP="50",
   MIN_GROUP_N="1",
   SEED="7092042",
-  RESULT_TAG="SMOKE_100_V2"
+  RESULT_TAG="SMOKE_100_V3"
 )
-source("scripts/Woodchester_V7bM_samegroup_pressure_models_V2.R")
+source("scripts/Woodchester_V7bM_samegroup_pressure_models_NUMERIC_DUMMIES.R")
