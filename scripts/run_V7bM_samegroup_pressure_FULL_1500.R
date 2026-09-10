075908976230b9ee62e13d0b28ad88a426d968e6
Dr Dave Hudson <64405890+davehudson67@users.noreply.github.com>
2026-09-10 16:04:35 +0100

    Point same-group full run to corrected V2 model

--- scripts/run_V7bM_samegroup_pressure_FULL_1500.R
+++ scripts/run_V7bM_samegroup_pressure_FULL_1500.R
@@ -3,6 +3,6 @@ Sys.setenv(
   N_KEEP="100",
   MIN_GROUP_N="1",
   SEED="7092042",
-  RESULT_TAG="FULL_1500"
+  RESULT_TAG="FULL_1500_V2"
 )
-source("scripts/Woodchester_V7bM_samegroup_pressure_models_FACTORFIX.R")
+source("scripts/Woodchester_V7bM_samegroup_pressure_models_V2.R")
