9f949ea62fdd1fb50699caa4f6ace961f58b8f6d
Dr Dave Hudson <64405890+davehudson67@users.noreply.github.com>
2026-09-11 11:53:29 +0100

    Add plain-language wrapper for infected mover audit

---
+++ scripts_current/20_audit_infected_movers_as_spreaders.R
@@ -0,0 +1,4 @@
+# First audit for the next analysis question:
+# do infected high-mobility badgers, especially Super excretors, have enough
+# observed movement and destination-group support to test onward spread?
+source("scripts/run_V7c_infected_movers_spreader_audit.R")
